7a9cd4c84adb70d93895440dd29ba6020f48dc17
kay1403 <angekoumba754@gmail.com>
2025-07-16 23:33:03 +0200

    Update utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -1,26 +1,9 @@
-from django_redis import get_redis_connection
-import logging
+from django.core.cache import cache
+from .models import Property
 
-logger = logging.getLogger(__name__)
-
-def get_redis_cache_metrics():
-    conn = get_redis_connection("default")
-    info = conn.info()
-
-    hits = info.get("keyspace_hits", 0)
-    misses = info.get("keyspace_misses", 0)
-    total_requests = hits + misses
-
-    if total_requests > 0:
-        hit_ratio = (hits / total_requests) * 100
-    else:
-        hit_ratio = 0.0
-
-    metrics = {
-        "hits": hits,
-        "misses": misses,
-        "hit_ratio": f"{hit_ratio:.2f}%"  # format en string pour affichage lisible
-    }
-
-    logger.info("Redis Cache Metrics: %s", metrics)
-    return metrics
+def getallproperties():
+    properties = cache.get('allproperties')
+    if not properties:
+        properties = list(Property.objects.all().values())
+        cache.set('allproperties', properties, 3600)  # 3600 secondes = 1 heure
+    return properties
