ed1e347551d642494121875d988e77c813770c80
Ange Koumba <angekoumba754@gmail.com>
2025-07-16 23:15:19 +0200

    Initial commit: Django project with Docker, PostgreSQL, Redis, and caching

---
+++ properties/views.py
@@ -0,0 +1,8 @@
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .utils import get_all_properties
+
+@cache_page(60 * 15)  
+def property_list(request):
+    properties = get_all_properties()  
+    return JsonResponse(properties, safe=False)
