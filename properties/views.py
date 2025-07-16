c8522184d49d3efa9686d67b1ca764f9d4040b1f
kay1403 <angekoumba754@gmail.com>
2025-07-16 23:37:56 +0200

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,8 @@
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .utils import getallproperties
+from .utils import get_all_properties
 
-@cache_page(60 * 15)
+@cache_page(60 * 15)  # Cache la réponse complète pendant 15 minutes
 def property_list(request):
-    properties = getallproperties()
+    properties = get_all_properties()  # Utilise le cache bas niveau pour la queryset
     return JsonResponse(properties, safe=False)
