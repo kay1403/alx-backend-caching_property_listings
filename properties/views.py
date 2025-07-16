a245f4124fc1887e0dbb1f2a05f137d4db9cf82a
kay1403 <angekoumba754@gmail.com>
2025-07-16 23:28:08 +0200

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,9 @@
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .utils import get_all_properties
+from .utils import get_all_properties  # or directly query Property
 
-@cache_page(60 * 15)  
+@cache_page(60 * 15)
 def property_list(request):
-    properties = get_all_properties()  
+    properties = get_all_properties()  # returns list of dicts
+    # Return the list directly, safe=False allows top-level arrays
     return JsonResponse(properties, safe=False)
